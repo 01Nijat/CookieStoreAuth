b0c174a674223334d0d005eab4103d9ecbe4cd5f
01Nijat <mr.nijat.asgarov@gmail.com>
2023-01-06 02:14:32 +0400

    Complete

---
+++ aspnet_cookieauth/Models/UserLoginModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aspnet_cookieauth.Models
+{
+    public class UserLoginModel
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public bool RememberMe { get; set; }
+    }
+}
